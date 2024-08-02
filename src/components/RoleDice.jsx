a1d289ebacb57fbd16c18402762cae32488a3be4
rajat bhatt <bettertryingtobecome@gmail.com>
2024-08-02 05:55:01 +0530

    commit
    image path

--- src/components/RoleDice.jsx
+++ src/components/RoleDice.jsx
@@ -34,11 +34,10 @@ const Button = styled.button`
 const RoleDice = ({ currentDice, roleDice }) => {
   return (
     <DiceContainer>
-      <DiceImage src={`/public\images\RoleDice\Dice${currentDice}.png`} alt={`Dice ${currentDice}`} />
+      <DiceImage src={`${process.env.PUBLIC_URL}/images/RoleDice/Dice${currentDice}.png`} alt={`Dice ${currentDice}`} />
       <Button onClick={roleDice}>Roll Dice</Button>
     </DiceContainer>
   );
 };
-
 
 export default RoleDice;
